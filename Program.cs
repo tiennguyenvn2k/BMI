5e97509eb3d88e031e5fc01921e133b7b4f56d54
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-10 19:42:59 +0700

    hoan thanh bai tap

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double weight,height;
+        Console.WriteLine("Moi nhap can nang (kg):");
+        weight = double.Parse(Console.ReadLine());
+        Console.WriteLine("Moi nhap chieu cao (m):");
+        height = double.Parse(Console.ReadLine());
+        double bmi = weight/(height*height);
+        bmi = Math.Round(bmi,1);
+        if(bmi<18.5)
+        {
+            Console.WriteLine("Underweight");
+        }
+        else if(bmi<25.0)
+        {
+            Console.WriteLine("Normal");
+        }
+        else if(bmi<30)
+        {
+            Console.WriteLine("Overweight");
+        }
+        else
+        {
+            Console.WriteLine("Obese");
+        }
+    }
+}
